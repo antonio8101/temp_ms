fd8c3c6a1accbf66cd53d985221e09c3604490a5
Antonio Bruno <abruno@artiso.com>
2023-01-26 21:05:51 +0100

    The only thing to do!

---
+++ test.js
@@ -0,0 +1,15 @@
+const assert = require("assert");
+
+function convertitore(p1, p2, p3){
+    return 0;
+}
+
+describe('test_fixture', function(){
+    it('testName_1', function(){
+        assert.equal(1,1);
+    });
+    it('testName_2', function(){
+        assert.equal(1,convertitore());
+    });
+});
+
